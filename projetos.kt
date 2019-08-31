a259166bb2432b2107a7b2cae2e73b869962b2f1
Samuel Linkon <35925156+samuellinkon30@users.noreply.github.com>
2019-08-31 19:21:53 -0300

    Sala

--- projetos.kt
+++ projetos.kt
@@ -1,94 +1,108 @@
 fun main ()
 {
-    val no1= No(1,No(2,No(3,null)))
-    val no2= No(4,No(5,No(6,null)))
-    println("Tamanho: ${tamanho(no1)}")
-    println("Valor: ${valor(no1)}")
-    println("Proximo: ${proximo(no1)}")
-    println("Concatenar: ${conc(no1,no2)}")
-    println("No n1= ${no1}")
-    println("N-esimo valor: ${enes(2,no1)}")
-    println("verifca valor 2: ${verfica(2,no1)}")
-    println("verifca valor 10: ${verfica(10,no1)}")
+    println("Fatorial: "+ fatororial(5))
+    println("Soma: "+ soma(6))
+    println("Potencia: 2ˆ4 = "+ exp(2,4))
+    println("Potencia: 2ˆ0 = "+ exp(2,0))
+    println("Potencia: 2ˆ1 = "+ exp(2,1))
+    println("Retorna Any =  ${imp("Samuel")}")
+    println("Funcao Reduzida: ( fun menor () = valor ) = ${menor()}")
 
+    //Listas Encadeadas
+    val l1 = No(1,null)
+    // No [ 1,No [ 2, No [3,null] ] ]
+    val l2 = incrementa(l1)
+    println("l1 = ${l1}")
+    println("l2 = ${l2}")
+    println("Tamanho: ${tamanho(l1)}")
+    l1.proximo()?.valor()
 }
-
-data class No (val valor:Int, val prox:No?)
-
+//Listas Encadeadas
+data class No(val info:Int, val prox:No?)
+{
+    fun valor():Unit
+    {
+        println("Valor: ${this.info}")
+    }
+    fun proximo(): No?
+    {
+        return prox
+    }
+}
+//Tamanho de uma lista encadeada
 fun tamanho(no:No?):Int
 {
     if (no==null)
     {
         return 0
-    }
-    else
+    } else
     {
-        return (1 + tamanho(no.prox))
+        return 1+(tamanho(no.prox))
     }
 }
 
-fun valor(no:No?):Int? {
-    if (no==null)
-    {
-        return null
-    }
-    else
-    {
-        return no.valor
-    }
-}
-
-fun proximo(no:No?):No?
+//Concatenar Listas
+fun concatenar(no1:No?, no2:No?):No?
 {
-    if(no==null)
+    val aux=null;
+    if(no1!=null && no2!==null)
     {
-        return null;
+        if(no1.prox==null)
+        {
+            return No(no1.info, no2)dwwdwddsdadadasdasdasdasdasdasdasd
+
+                    dlsoddlsdate dadaadadddddddd
+        }
+        else
+        {
+            return (concatenar(no1.prox,no2))
+        }
     }
     else
-    {
-        return no.prox;
+        return no1;
+}
+
+//Incrementa o valor dos Nós
+fun incrementa(p: No?):No? =
+    if (p == null)
+        null
+    else {
+        No(p.info + 1, incrementa(p.prox))
+    }
+
+//Funcoes não precisam ter return se os seu retorno for de um unico valor.
+val valor=10
+fun menor () = valor
+
+//Funcoes do tipo Any com Objetos do Tipo Any Retornam "Qualquer coisa."
+fun imp(x:Any): Any {return x}
+
+// Funcao Fatorial
+fun fatororial (num:Int):Int
+{
+    if (num==0) return 1
+    else if (num==1) return 1
+    else return (num*fatororial(num-1))
+}
+// Funcao Somatorio
+fun soma (num:Int):Int
+{
+    if(num==0) { return 0 }
+    else if (num==1)
+    { return 1}
+    else {
+        return num+soma(num-1)
     }
 }
 
-fun conc(no1:No?,no2:No?):No?
+// Funcao Potencial
+fun exp(num1:Int, num2:Int):Int
 {
-    if (no1==null)
-    {
-        return no2
-    } else if ( no2==null)
-    {
-        return no1
-    }
-    else if (no1.prox==null)
-    {
-        return No(no1.valor,no2)
-    }
+    if(num2==0) {return 1}
+    else if (num2==1) {return  num1}
     else
     {
-        return No(no1.valor ,conc(no1.prox,no2))
+        return num1 *(exp(num1,num2-1))
     }
 }
 
-fun enes(x:Int,no:No?):Int?
-{
-    if(x==1)
-    {
-        return no?.valor
-    }
-    else
-    {
-        return enes(x-1,no?.prox)
-    }
-}
-
-fun verfica(x:Int,no:No?):Boolean
-{
-    if (x==no?.valor)
-    {
-        return true
-    }
-    else
-    {
-        return false && (verfica(x,no?.prox))
-    }
-}
